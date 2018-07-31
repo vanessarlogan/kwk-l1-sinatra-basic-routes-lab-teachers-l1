176d3310faeb24d243f4a4cb9b8f7296b4759eb8
vanessarlogan Wentzloff <vanessarlogan@gmail.com>
2018-07-31 16:57:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  #homepage- whatever my local host is
   get '/' do
     "Hello World"
   end
+
   
   get '/name' do
     "My name is Vanessa"
